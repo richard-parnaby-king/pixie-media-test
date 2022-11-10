5b5e95da4e164f930c2141f941ba155d98649cf7
Richard Parnaby-King <r.parnaby-king@suttonsilver.co.uk>
2022-11-10 20:53:24 +0000

    admin index

---
+++ src/Ui/Component/Listing/Column/ApiActions.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types=1);
+
+namespace RichardParnabyKing\PixieMediaTest\Ui\Component\Listing\Column;
+
+class ApiActions extends \Magento\Ui\Component\Listing\Columns\Column
+{
+
+    const URL_PATH_DELETE = 'richardparnabyking_pixiemediatest/api/delete';
+    const URL_PATH_EDIT = 'richardparnabyking_pixiemediatest/api/edit';
+    const URL_PATH_DETAILS = 'richardparnabyking_pixiemediatest/api/details';
+    protected $urlBuilder;
+
+    /**
+     * @param \Magento\Framework\View\Element\UiComponent\ContextInterface $context
+     * @param \Magento\Framework\View\Element\UiComponentFactory $uiComponentFactory
+     * @param \Magento\Framework\UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\UiComponent\ContextInterface $context,
+        \Magento\Framework\View\Element\UiComponentFactory $uiComponentFactory,
+        \Magento\Framework\UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                if (isset($item['api_id'])) {
+                    $item[$this->getData('name')] = [
+                        'edit' => [
+                            'href' => $this->urlBuilder->getUrl(
+                                static::URL_PATH_EDIT,
+                                [
+                                    'api_id' => $item['api_id']
+                                ]
+                            ),
+                            'label' => __('Edit')
+                        ],
+                        'delete' => [
+                            'href' => $this->urlBuilder->getUrl(
+                                static::URL_PATH_DELETE,
+                                [
+                                    'api_id' => $item['api_id']
+                                ]
+                            ),
+                            'label' => __('Delete'),
+                            'confirm' => [
+                                'title' => __('Delete "${ $.$data.title }"'),
+                                'message' => __('Are you sure you want to delete a record?')
+                            ]
+                        ]
+                    ];
+                }
+            }
+        }
+        
+        return $dataSource;
+    }
+}
+
